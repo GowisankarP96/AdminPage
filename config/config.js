786a3062dd572c4a19697d018ea91f6695ed280a
Gowrisankar P <psankar.gr@gmail.com>
2025-05-26 15:34:23 +0530

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,11 @@
+
+const sessionSecret = "mysitesessionsecret";
+
+const emailUser="sankar.gw77@gmail.com"
+const emailPassword="pzef syqd kgik sjzc"
+
+module.exports={
+sessionSecret,
+emailUser,
+emailPassword
+}
